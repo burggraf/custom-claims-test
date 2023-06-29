fa078b102e1c0fae72c9985d8438c912b71e703e
Mark Burggraf <markb@mantisbible.com>
2023-06-29 16:58:07 -0700

    cleaner db_pre_request function

--- data/db_pre_request.sql
+++ data/db_pre_request.sql
@@ -1,24 +1,23 @@
-
 CREATE OR REPLACE FUNCTION db_pre_request() RETURNS JSONB
-    LANGUAGE "plpgsql" SECURITY DEFINER SET search_path = public
-    AS 
-    $$
+    LANGUAGE plpgsql SECURITY DEFINER SET search_path = public AS 
+$$
     DECLARE 
-    claims JSONB;
-    user_role TEXT;
+      claims JSONB;
+      user_role TEXT;
     BEGIN
+    -- get the claims from the JWT 
     select coalesce(current_setting('request.jwt.claims', true),'{}') into claims;
-    claims := claims || 
-        jsonb_build_object('expires',to_timestamp(coalesce((claims::JSONB)->'exp','0')::numeric));
+    -- convert the exp claim to a timestamp with time zone (UTC)
+    -- claims := claims || 
+    --     jsonb_build_object('expires',to_timestamp(coalesce((claims::JSONB)->'exp','0')::numeric));
 
+    -- ******************************** CUSTOM CLAIMS ********************************
     -- get user_role from the roles table and add it to the claims
     select role from roles where uid = ((claims::JSONB)->>'sub')::uuid into user_role;
-    claims := claims ||
-        jsonb_build_object('user_role',user_role);
+    claims := claims || jsonb_build_object('user_role',user_role);
+    -- *******************************************************************************
 
-    PERFORM set_config(
-        'request.jwt.other'::text, claims::text, false /* is_local */
-    );
+    PERFORM set_config('request.jwt.other'::text, claims::text, false /* is_local */);
 
     return claims;
     END;
